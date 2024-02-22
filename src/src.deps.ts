90404e338e8e1369eb1ed7876946f7f37cf79938
mcgear <michael.gearhardt@gmail.com>
2024-02-22 10:27:00 -0700

    f updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -10,8 +10,8 @@ import Mime from 'npm:mime';
 export const mime = Mime;
 
 // export * from '../../reference-architecture/mod.ts';
-export * from 'https://deno.land/x/fathym_common@v0.0.152/mod.ts';
+export * from 'https://deno.land/x/fathym_common@v0.0.153/mod.ts';
 // export * from '../../everything-as-code/mod.ts';
-export * from 'https://deno.land/x/fathym_everything_as_code@v0.0.325-integration/mod.ts';
+export * from 'https://deno.land/x/fathym_everything_as_code@v0.0.327-integration/mod.ts';
 
 import 'https://deno.land/std@0.216.0/dotenv/load.ts';
