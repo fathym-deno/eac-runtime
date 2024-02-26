80006407f6c36efa6929f59605b05e0d22d5dbc3
mcgear <michael.gearhardt@gmail.com>
2024-02-25 23:17:39 -0700

    ref fix

--- src/install/install.deps.ts
+++ src/install/install.deps.ts
@@ -3,5 +3,5 @@ export { parseArgs } from 'https://deno.land/std@0.216.0/cli/mod.ts';
 export { toText } from 'https://deno.land/std@0.216.0/streams/to_text.ts';
 
 export * from 'https://deno.land/x/fathym_common@v0.0.157/src/utils/path/exists.ts';
-export * from 'https://deno.land/x/fathym_common@v0.0.157/src/utils/object.helpers.ts';
+export * from 'https://deno.land/x/fathym_common@v0.0.157/src/utils/merge.helpers.ts';
 export * from 'https://deno.land/x/fathym_everything_as_code@v0.0.338-integration/src/utils/library.helpers.ts';
