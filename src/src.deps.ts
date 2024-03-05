0bb8c45939df069616436ba64a9dc87869f6102a
mcgear <michael.gearhardt@gmail.com>
2024-03-05 12:28:24 -0700

    ef updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -37,8 +37,8 @@ export { VectorStore } from 'npm:@langchain/core/vectorstores';
 // export * from '../../reference-architecture/mod.ts';
 export * from 'https://deno.land/x/fathym_common@v0.0.160/mod.ts';
 // export * from '../../everything-as-code/mod.ts';
-export * from 'https://deno.land/x/fathym_everything_as_code@v0.0.374/mod.ts';
+export * from 'https://deno.land/x/fathym_everything_as_code@v0.0.375/mod.ts';
 // export * from '../../everything-as-code-api/mod.ts';
-export * from 'https://deno.land/x/fathym_everything_as_code_api@v0.0.10/mod.ts';
+export * from 'https://deno.land/x/fathym_everything_as_code_api@v0.0.18/mod.ts';
 
 import 'https://deno.land/std@0.216.0/dotenv/load.ts';
