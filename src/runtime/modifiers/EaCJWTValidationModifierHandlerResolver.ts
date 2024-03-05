5ab2729c69f94b58f94728353b6a64974eaee71e
mcgear <michael.gearhardt@gmail.com>
2024-03-05 13:01:11 -0700

    is build check for jwt validation

--- src/runtime/modifiers/EaCJWTValidationModifierHandlerResolver.ts
+++ src/runtime/modifiers/EaCJWTValidationModifierHandlerResolver.ts
@@ -3,11 +3,16 @@ import {
   EaCJWTValidationModifierDetails,
   isEaCJWTValidationModifierDetails,
 } from '../../src.deps.ts';
+import { IS_BUILDING } from '../../constants.ts';
 import { ModifierHandlerResolver } from './ModifierHandlerResolver.ts';
 import { establishJwtValidationMiddleware } from '../../modules/jwt/jwtValidationMiddleware.ts';
 
 export const EaCJWTValidationModifierHandlerResolver: ModifierHandlerResolver = {
   Resolve(_ioc, modifier) {
+    if (IS_BUILDING) {
+      return Promise.resolve(undefined);
+    }
+
     if (!isEaCJWTValidationModifierDetails(modifier.Details)) {
       throw new Deno.errors.NotSupported(
         'The provided modifier is not supported for the EaCJWTValidationModifierHandlerResolver.',
