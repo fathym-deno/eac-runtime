c39dc833ea8ceac5945ad6fa967d83e1ba17c349
mcgear <michael.gearhardt@gmail.com>
2024-03-15 03:48:31 -0600

    slug

--- src/install/files/apps/api/[slug]/another.ts
+++ src/install/files/apps/api/[slug]/another.ts
@@ -1,7 +1,7 @@
 import { EaCRuntimeHandlers } from '@fathym/eac/runtime';
 
 export default {
-  GET(_req, _ctx) {
-    return Response.json({ Hello: 'WorldSubAnother' });
+  GET(_req, ctx) {
+    return Response.json({ Hello: `World${ctx.Params.slug}Another` });
   },
 } as EaCRuntimeHandlers;
