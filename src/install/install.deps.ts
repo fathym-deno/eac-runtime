07da4e43f7b295b7563813b9cf85faf8b6967cf5
mcgear <michael.gearhardt@gmail.com>
2024-06-14 09:50:47 -0600

    cleanup refs

--- src/install/install.deps.ts
+++ src/install/install.deps.ts
@@ -1,9 +1,9 @@
-export * from '../../deps.ts';
-import * as _parse from 'npm:pdf-parse';
-import * as _azureSearch from 'npm:@azure/search-documents';
+// export * from '../../deps.ts';
+// import * as _parse from 'npm:pdf-parse';
+// import * as _azureSearch from 'npm:@azure/search-documents';
 export { parseArgs } from 'https://deno.land/std@0.220.1/cli/mod.ts';
 export { toText } from 'https://deno.land/std@0.220.1/streams/to_text.ts';
 
-// export * from 'https://deno.land/x/fathym_common@v0.0.184/src/utils/path/exists.ts';
-// export * from 'https://deno.land/x/fathym_common@v0.0.184/src/utils/merge.helpers.ts';
+export * from 'https://deno.land/x/fathym_common@v0.0.184/src/utils/path/exists.ts';
+export * from 'https://deno.land/x/fathym_common@v0.0.184/src/utils/merge.helpers.ts';
 export * from 'https://deno.land/x/fathym_everything_as_code@v0.0.413/src/utils/library.helpers.ts';
