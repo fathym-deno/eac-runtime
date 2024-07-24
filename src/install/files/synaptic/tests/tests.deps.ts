32c696bf7074127bd6613caf31acac8aed34dc5b
mcgear <michael.gearhardt@gmail.com>
2024-07-24 14:40:44 -0600

    imports

--- src/install/files/synaptic/tests/tests.deps.ts
+++ src/install/files/synaptic/tests/tests.deps.ts
@@ -1,3 +1,7 @@
+import * as _azureSearch from 'npm:@azure/search-documents';
+import * as _parse from 'npm:pdf-parse';
+import * as _htmlToText from 'npm:html-to-text';
+
 export * from 'https://deno.land/std@0.224.0/assert/mod.ts';
 
 export * from '@fathym/eac';
